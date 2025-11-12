f545753ace8e9f61aa1f8045398e0629fe69a271
wouterlanduit <14540199+wouterlanduit@users.noreply.github.com>
2025-11-12 18:06:02 +0100

    add navigation bar

--- financemanagementwebclient/src/App.tsx
+++ financemanagementwebclient/src/App.tsx
@@ -1,16 +1,19 @@
 import { FluentProvider, webDarkTheme } from '@fluentui/react-components';
 import './App.css'
 import MonthlyDetailsGrid from './components/MonthlyDetailsGrid/MonthlyDetailsGrid'
+import NavBar from './components/NavBar/NavBar'
 import { AIPDataSource, type DataSource } from './services/data-source-service'
 
 function App() {
     const ds: DataSource = new AIPDataSource();
     ds.useDummyData = window.location.hostname === 'localhost';
 
+    // TODO: react router
     return (
         <>
             <FluentProvider theme={webDarkTheme}>
                 <div>
+                    <NavBar />
                     <MonthlyDetailsGrid
                         name="September"
                         source={ds}
---
+++ financemanagementwebclient/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import './NavBar.css'
+function NavBar() {
+
+    return <nav>
+        <ul>
+            <li>Home</li>
+            <li>Expenses</li>
+            <li>Setup</li>
+        </ul>
+    </nav>
+}
+
+export default NavBar;
